d20f6c6a738efb7d18de4de041413435945cc560
Carynk <caryn@carynk.com>
2015-08-10 14:15:59 -0700

    Added functionality for 5 guesses.

--- game.js
+++ game.js
@@ -2,12 +2,7 @@
 <script>
 'use strict';
 
-var question
-var answer
-var guess
-var message
-
-Game = function (question, answer, guess, message) {
+Game = function (question, answer) {
 	this.question = question;
 	this.answer = answer;
 	this.guess = 0;
@@ -15,33 +10,41 @@ Game = function (question, answer, guess, message) {
 	this.playGame = function() {
 		this.guess = prompt(this.question);
 
-		for ( var i = 0; i < 26)
+		for ( var i = 1; i < 5; i++) {
 
-		
-		alert(this.message);
-		if (this.guess < this.answer) {
-			this.message = 'I am offended that you think so little of me.';
-		} else if (this.guess > this.answer) {
-			this.message = 'Whoa, it\'s a cookie, not a candy bar, man!';
-		} else {  
-			this.message = 'Dead on! Got any milk?';
-		}
+			
+			if (this.guess < this.answer) {
+				this.message = 'I am offended that you think so little of me. This was guess ' + i + ' of 5';
+				alert(this.message);
+				this.guess=prompt("Try again");
+			} else if (this.guess > this.answer) {
+				this.message = 'Whoa, it\'s a cookie, not a candy bar, man! This was guess ' + i + ' of 5';
+				alert(this.message);
+				this.guess=prompt("Try again");
+			} else {  
+				this.message = 'You read my mind! Got any milk?';
+				alert(this.message);
+				break;
+			}
+		}	
+		alert('Thank you for playing.');
 	}	
-};
+}
+
 var firstGame = new Game('Guess how many chocolate chips are in my imaginary cookie!', 27);	
 firstGame.playGame();
 </script>
 
 // Friday - notes off of class sample:
-var firstHint = document.getElementById('first-hint')
-first.Hint.innerHTML = hint1;
+//var firstHint = document.getElementById('first-hint')
+//first.Hint.innerHTML = hint1;
 
-function addToHintList() {
-	var hintListItem = document.createELement('li'):
-	var hintNode = document.createTextNode(hint1)
-	hintListItem.appendChild'(hintNode);
-	document.getElementById('hint-list').appendChild(hintListItem);('')
-}
+//function addToHintList() {
+//	var hintListItem = document.createELement('li'):
+//	var hintNode = document.createTextNode(hint1)
+//	hintListItem.appendChild'(hintNode);
+//	document.getElementById('hint-list').appendChild(hintListItem);('')
+//}
 
 
 
